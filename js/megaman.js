f15f2f970af68f360036ee89fdbe149288974482
illl48 <mruncanny@gmail.com>
2015-09-13 19:04:57 -0700

    jhub

--- js/megaman.js
+++ js/megaman.js
@@ -1,13 +1,35 @@
 function megamanscript(){
 
- var background=document.getElementById("ground");
- var id = setInterval(function(){
-   background.style.left=""+(parseInt(window.getComputedStyle(background).getPropertyValue('left').replace("px", ""))-5)+"px";
-     console.log("run");
- },100);   
+var background=document.getElementById("ground");
+var city=document.getElementById("city");
+// speed in milliseconds
+var bscrollSpeed = 5;
+var cscrollSpeed = 20;    
+// set the default position
+var bcurrent = 0;
+var ccurrent = 0;    
+
+function bbgscroll() {
+   // 1 pixel row at a time
+  console.log("b movin");    
+   bcurrent -= 1;
+   background.style.backgroundPosition=bcurrent + "px 0";    
+}    
+
+function cbgscroll() {
+   // 1 pixel row at a time
+  console.log("c movin");    
+   ccurrent -= 1;
+   city.style.backgroundPosition=ccurrent + "px 0";    
+}      
     
+ var bid = setInterval(bbgscroll,bscrollSpeed);   
+ var cid = setInterval(cbgscroll,cscrollSpeed);    
     
 };
 
-//window.onload = megamanscript;
+window.onload = megamanscript;
 
+
+	
+		 
