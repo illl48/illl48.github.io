0fdc5ead0f20d16f8d44540d7ff5ee279cca3624
illl48 <mruncanny@gmail.com>
2015-12-17 15:57:55 -0800

    iphone

---
+++ js/directives/listDirective.js
@@ -0,0 +1,19 @@
+app.directive('resize', ['$window' 
+  function($window) {
+    return {
+      restrict: 'A',
+      scope: {
+      },
+      link: function(scope, ele, attrs) {
+          $window.onresize = function() {
+            scope.$apply();
+          };
+
+          scope.$watch(function() {
+              return angular.element($window)[0].innerWidth;
+          }, function() {
+              console.log("window size: "+angular.element($window)[0].innerWidth);
+              scope.$emit("window_size",angular.element($window)[0].innerWidth); 
+          });
+      }}
+}])
