d4facb65e03e1225a71aa5fdeade72ff1d63eca8
illl48 <mruncanny@gmail.com>
2015-12-17 00:47:45 -0800

    add test button

--- js/controllers/firstpageController.js
+++ js/controllers/firstpageController.js
@@ -4,7 +4,7 @@ app.controller('firstpageController', ['$scope','$http', function($scope,$http) 
     
     $scope.send=function(){
         //https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522,151.1957362&radius=500&types=food&name=cruise&key=AIzaSyAMOszNOH-Bf1QX68qGtSD_9QGmJDLlP1g
-        $http.get("https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522,151.1957362&radius=500&types=food&name=cruise&key=AIzaSyAMOszNOH-Bf1QX68qGtSD_9QGmJDLlP1g")
+        $http.jsonp("https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522,151.1957362&radius=500&types=food&name=cruise&key=AIzaSyAMOszNOH-Bf1QX68qGtSD_9QGmJDLlP1g")
             .error(function(error){
                    console.log("error: "+error);
             })
