ea73848a8374112b7c2dfa6606ac9291bb267010
illl48 <mruncanny@gmail.com>
2015-12-17 00:44:48 -0800

    add test button

--- js/controllers/firstpageController.js
+++ js/controllers/firstpageController.js
@@ -1,4 +1,16 @@
-app.controller('firstpageController', ['$scope', function($scope) {
+app.controller('firstpageController', ['$scope','$http', function($scope,$http) {
     $scope.prewelcome = "Press";
     $scope.postwelcome = "to start.";
+    
+    $scope.send=function(){
+        //https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522,151.1957362&radius=500&types=food&name=cruise&key=AIzaSyAMOszNOH-Bf1QX68qGtSD_9QGmJDLlP1g
+        $http.get("https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522,151.1957362&radius=500&types=food&name=cruise&key=AIzaSyAMOszNOH-Bf1QX68qGtSD_9QGmJDLlP1g")
+            .error(function(error){
+                   console.log("error: "+error);
+            })
+            .success(function(res){
+                    console.log("res: "+res);
+            });
+    }
+    
 }]);
