d4fbb29f175dd50b89e8abebc908815aa7a90363
illl48 <mruncanny@gmail.com>
2015-12-19 14:58:37 -0800

    map to list

--- js/factories/movetoFactory.js
+++ js/factories/movetoFactory.js
@@ -3,12 +3,12 @@ app.factory('movetoF', ['$window',function($window){
 
     o.moveTo = function(listId) {
         
-        var top=document.getElementById(listId).getBoundingClientRect().top;
+        var top = document.getElementById(listId).getBoundingClientRect().top;
         var iH = angular.element($window)[0].innerHeight;
         var iW = angular.element($window)[0].innerWidth;   
         var totalpadding =(iW > 583)? Math.floor((iH-52)*0.3)+92 : (iW > 420)? 294 : 244;
-        var distance = (top - totalpadding === 0)? 1 : top - totalpadding;
-        var up = (distance > 0)? false : true;
+        var distance =(top - totalpadding === 0)? 1 : top - totalpadding;
+        var up =(distance > 0)? false : true;
         var interval;
           
         interval = setInterval(function(){
