82d406364bfea95a979afc9931ac1b022a504840
illl48 <mruncanny@gmail.com>
2015-12-19 15:15:34 -0800

    fixed

--- js/factories/movetoFactory.js
+++ js/factories/movetoFactory.js
@@ -6,7 +6,7 @@ app.factory('movetoF', ['$window',function($window){
         var top = document.getElementById(listId).getBoundingClientRect().top;
         var iH = angular.element($window)[0].innerHeight;
         var iW = angular.element($window)[0].innerWidth;   
-        var totalpadding =(iW > 583)? Math.floor((iH-52)*0.3)+92 : (iW > 420)? 294 : 244;
+        var totalpadding =(iW > 583)? Math.floor((iH-52)*0.3)+92 : (iW > 420)? 300 : 244;
         var distance =(top - totalpadding === 0)? 1 : top - totalpadding;
         var up =(distance > 0)? false : true;
         var interval;
