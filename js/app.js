e2f77958b06d83be2ba65391e605649172742157
illl48 <mruncanny@gmail.com>
2016-02-02 00:03:41 -0800

    fixed

--- js/app.js
+++ js/app.js
@@ -1,30 +1,3 @@
-<<<<<<< HEAD
-
-var app = angular.module('MyApp', ['ngRoute','ngMessages']);
-
-app.config(function ($routeProvider) {
-  $routeProvider
-    .when('/firstpage', {
-      controller: "firstpageController",
-      templateUrl: "views/firstpage.html"
-    })
-    .when('/yesterday', {
-      controller: "yesterdayController",
-      templateUrl: "views/yesterday.html"
-    })
-    .when('/projects', {
-      controller: "projectsController",
-      templateUrl: "views/projects.html"
-    })
-    .when('/aboutme', {
-      controller: "aboutmeController",
-      templateUrl: "views/aboutme.html"
-    })
-    .otherwise({
-      redirectTo: '/firstpage'
-    });
-});
-=======
 var app = angular.module('MyApp', ['ui.router','ngMap','ngAutocomplete']);
 
 app.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
@@ -41,5 +14,4 @@ app.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $ur
 }]);
 
 
->>>>>>> 5234c3f8ac3993a7d06ce59aff3e2b9673c328a4
 
